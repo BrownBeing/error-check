97d63e176fe6ddd050538bc0b21673d5885137fd
Mr Brown <brownasters@yahoo.com>
2024-07-07 13:02:28 +0000

    error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react' ;
 import './App.css';
+import NewApp from './NewApp';
+//import {Profile} from './NewApp';
+import data from "./data.json";
 
-function App() {
-  return (
+
+export default function App() {
+  const [pokemon, Setpokemon] =useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/client/data.json")
+    .then((res)=>res.json())
+    .then((data) => Setpokemon(data))
+    .catch((error)=> console.error("there is an error :", error))
+    }, []);
+  
+    return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input />
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Column1</th>
+            <th>Column2</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((individual, index)=>{
+            return(<tr key ={index}>
+              <td>{individual.name.english}</td>
+              <td>{individual.type.join(",")}</td>
+              </tr>)
+          })}
+          
+        </tbody>
+      </table>
+      
     </div>
   );
 }
 
-export default App;
+
---
+++ src/NewApp.js
@@ -0,0 +1,13 @@
+function Profile(){
+    return(
+        <img
+        src="https://i.imhur.com/MK3eW3Am.jpg"
+        alt="image"/>
+    )
+}
+
+export default function NewApp(){
+    return(
+        <Profile/>
+    )
+}
